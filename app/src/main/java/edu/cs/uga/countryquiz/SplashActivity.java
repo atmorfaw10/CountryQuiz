2a852f92da50e69e1ce337d7317ba1e3ba1f8124
atmorfaw10 <asongtmorfaw_sd10@users.noreply.github.com>
2021-04-01 22:35:56 -0400

    Initial commit

---
+++ app/src/main/java/edu/cs/uga/countryquiz/SplashActivity.java
@@ -0,0 +1,18 @@
+package edu.cs.uga.countryquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.black)));
+    }
+}
